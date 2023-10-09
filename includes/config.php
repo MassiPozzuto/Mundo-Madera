24e155b92fcbf437901380de99c85527ef45e559
gonzalol14 <luquegonzalo0714@gmail.com>
2023-10-09 13:46:36 -0300

    cambio bd y config

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 $servername = "localhost";
 $connectioninfo = array("Database" => "mundo-madera");
-$conn = sqlsrv_connect($servername, $connectioninfo);
+$conn = mysqli_connect($servername, $connectioninfo);
 
 if(!$conn){
     echo "La conexion no se pudo establecer.<br />";
