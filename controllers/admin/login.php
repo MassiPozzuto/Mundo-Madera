3c7af86d850d367a86890c07c6e0469229c574df
MassiPozzuto <massipozzuto2016@gmail.com>
2023-11-09 03:32:50 -0300

    agrego consulta preparada en login

--- controllers/admin/login.php
+++ controllers/admin/login.php
@@ -5,34 +5,51 @@ if (isset($_SESSION['user'])) {
     header('location: inicio.php');
 }
 
-if(!empty($_POST)){
-    if(!empty($_POST['username']) && !empty($_POST['password'])){
-        $sqlLogin = "SELECT * FROM usuarios 
-                        WHERE username='" . trim($_POST['username']) . "' AND usuarios.password='" . sha1($_POST['password']) . "' AND fecha_eliminacion IS NULL";
-        $resultLogin = mysqli_query($conn, $sqlLogin);
-    
-        if (mysqli_num_rows($resultLogin) === 1) {
-            $_SESSION['user'] = mysqli_fetch_assoc($resultLogin);
-    
-            if (isset($_POST['remember']) && $_POST['remember']) {
-                setcookie('username', $_POST['username'], time() + 20 * 86400, '/');
-                setcookie('password', sha1($_POST['password']), time() + 20 * 86400, '/');
-    
-                $_COOKIE['username'] = $_POST['username'];
-                $_COOKIE['password'] = $_POST['password'];
+if (!empty($_POST)) {
+    if (!empty($_POST['username']) && !empty($_POST['password'])) {
+        // Consulta preparada
+        $sqlLogin = "SELECT * FROM usuarios WHERE username=? AND usuarios.password=? AND fecha_eliminacion IS NULL";
+        $stmt = mysqli_prepare($conn, $sqlLogin);
+
+        if ($stmt) {
+            // Vincular parámetros
+            mysqli_stmt_bind_param($stmt, "ss", $username, $hashed_password);
+            // Asignar valores a los parámetros
+            $username = trim($_POST['username']);
+            $hashed_password = sha1($_POST['password']);
+            // Ejecutar la consulta
+            mysqli_stmt_execute($stmt);
+            // Obtener resultados
+            $resultLogin = mysqli_stmt_get_result($stmt);
+
+            // Verificar el número de filas
+            if (mysqli_num_rows($resultLogin) === 1) {
+                $_SESSION['user'] = mysqli_fetch_assoc($resultLogin);
+
+                if (isset($_POST['remember']) && $_POST['remember']) {
+                    setcookie('username', $_POST['username'], time() + 20 * 86400, '/');
+                    setcookie('password', sha1($_POST['password']), time() + 20 * 86400, '/');
+
+                    $_COOKIE['username'] = $_POST['username'];
+                    $_COOKIE['password'] = $_POST['password'];
+                }
+
+                //$message['message'] = "Se ha iniciado sesión correctamente";
+                header("Location: inicio.php");
+            } else {
+                // Nombre de usuario incorrecto 
+                //$message['username'] = "El nombre de usuario no es correcto";
+                $error_msj = "El nombre de usuario y/o la contraseña no son correctos.";
             }
-    
-            //$message['message'] = "Se ha iniciado sesión correctamente";
-            header("Location: inicio.php");
+
+            // Cerrar la consulta preparada
+            mysqli_stmt_close($stmt);
         } else {
-            // Nombre de usuario incorrecto 
-            //$message['username'] = "El nombre de usuario no es correcto";
-            $error_msj = "El nombre de usuario y/o la contraseña no son correctos.";
+            // Manejar el error de la consulta preparada
+            $error_msj = "Error al iniciar sesión. Intentelo más tarde";
         }
     } else {
-        //header("Content-Type: application/json; charset=utf-8");
-        //return print_r(json_encode($message));
-        //Campos vacios
+        // Campos vacíos
         $error_msj = "Debe ingresar un nombre de usuario y una contraseña.";
     }
 }
