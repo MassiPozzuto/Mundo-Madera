7c9973bba16109f74fee770f7ab032841eb79fcc
MassiPozzuto <massipozzuto2016@gmail.com>
2023-11-09 03:17:52 -0300

    Arreglo errores logicos y agrego FKs

--- db/mundo-madera.sql
+++ db/mundo-madera.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-10-2023 a las 18:43:52
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 09-11-2023 a las 07:17:25
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,9 +30,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL,
   `tipo` varchar(50) NOT NULL,
-  `fecha_creacion` datetime NOT NULL,
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
   `fecha_eliminacion` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `tipo`, `fecha_creacion`, `fecha_eliminacion`) VALUES
+(4, 'Cedro', '2023-11-08 04:39:01', '2023-11-08 04:39:01');
 
 -- --------------------------------------------------------
 
@@ -44,7 +51,7 @@ CREATE TABLE `envios` (
   `id` int(11) NOT NULL,
   `id_pedido` int(11) NOT NULL,
   `direccion` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -60,9 +67,9 @@ CREATE TABLE `pedidos` (
   `telefono` int(11) NOT NULL,
   `tipo` varchar(15) NOT NULL,
   `estado` varchar(15) NOT NULL,
-  `fecha_creacion` datetime NOT NULL,
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
   `fecha_entrega` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -73,7 +80,7 @@ CREATE TABLE `pedidos` (
 CREATE TABLE `pedidos_productos` (
   `id_pedido` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -87,9 +94,17 @@ CREATE TABLE `productos` (
   `nombre` varchar(50) NOT NULL,
   `stock` smallint(6) DEFAULT NULL,
   `precio` float NOT NULL,
-  `fecha_creacion` int(11) NOT NULL,
-  `fecha_eliminacion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
+  `fecha_eliminacion` datetime DEFAULT NULL,
+  `rutimapro` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `id_categoria`, `nombre`, `stock`, `precio`, `fecha_creacion`, `fecha_eliminacion`, `rutimapro`) VALUES
+(1, 4, 'Prueba', 15, 5000, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '20231108001858.jpg');
 
 -- --------------------------------------------------------
 
@@ -99,13 +114,20 @@ CREATE TABLE `productos` (
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `apellido` varchar(30) DEFAULT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(40) NOT NULL,
-  `fecha_creacion` int(11) NOT NULL,
-  `fecha_eliminacion` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
+  `fecha_eliminacion` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `username`, `password`, `fecha_creacion`, `fecha_eliminacion`) VALUES
+(1, NULL, NULL, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', '2023-11-09 01:51:51', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -121,7 +143,8 @@ ALTER TABLE `categorias`
 -- Indices de la tabla `envios`
 --
 ALTER TABLE `envios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_envios__pedidos` (`id_pedido`);
 
 --
 -- Indices de la tabla `pedidos`
@@ -130,10 +153,18 @@ ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `pedidos_productos`
+--
+ALTER TABLE `pedidos_productos`
+  ADD KEY `fk_pedidos_productos__pedidos` (`id_pedido`),
+  ADD KEY `fk_pedidos_productos__productos` (`id_producto`);
+
+--
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_productos__categorias` (`id_categoria`) USING BTREE;
 
 --
 -- Indices de la tabla `usuarios`
@@ -149,7 +180,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `envios`
@@ -167,13 +198,36 @@ ALTER TABLE `pedidos`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `envios`
+--
+ALTER TABLE `envios`
+  ADD CONSTRAINT `fk_envios__pedidos` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`);
+
+--
+-- Filtros para la tabla `pedidos_productos`
+--
+ALTER TABLE `pedidos_productos`
+  ADD CONSTRAINT `fk_pedidos_productos__pedidos` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`),
+  ADD CONSTRAINT `fk_pedidos_productos__productos` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `fk_productos_categorias` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
