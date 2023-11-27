eb8f627cfc33acf16e8f34e47d0b66859a75e473
MassiPozzuto <massipozzuto2016@gmail.com>
2023-11-27 09:11:15 -0300

    actualizo db

--- db/mundo-madera.sql
+++ db/mundo-madera.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-11-2023 a las 07:17:25
+-- Tiempo de generación: 27-11-2023 a las 13:10:51
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -29,7 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL,
-  `tipo` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(150) DEFAULT NULL,
   `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
   `fecha_eliminacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -38,8 +39,36 @@ CREATE TABLE `categorias` (
 -- Volcado de datos para la tabla `categorias`
 --
 
-INSERT INTO `categorias` (`id`, `tipo`, `fecha_creacion`, `fecha_eliminacion`) VALUES
-(4, 'Cedro', '2023-11-08 04:39:01', '2023-11-08 04:39:01');
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`, `fecha_creacion`, `fecha_eliminacion`) VALUES
+(1, 'Roble', NULL, '2023-11-17 00:49:43', NULL),
+(2, 'Pinooo', 'Tipo de arbol', '2023-11-17 00:49:43', NULL),
+(4, 'Cedro', NULL, '2023-11-08 04:39:01', NULL),
+(5, 'Nogal', NULL, '2023-11-22 20:25:34', NULL),
+(7, 'Quebracho Blanco', NULL, '2023-11-25 02:01:33', '2023-11-25 02:15:33'),
+(8, 'Lo siento pero já', 'Esto es una prueba\r\n', '2023-11-25 02:49:37', NULL),
+(15, 'Prueba de categoria :)', 'Esto es una segunda prueba.', '2023-11-25 06:34:45', '2023-11-25 06:53:49');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_producto`
+--
+
+CREATE TABLE `categoria_producto` (
+  `id_categoria` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categoria_producto`
+--
+
+INSERT INTO `categoria_producto` (`id_categoria`, `id_producto`) VALUES
+(1, 40),
+(2, 39),
+(2, 40),
+(4, 37),
+(8, 39);
 
 -- --------------------------------------------------------
 
@@ -50,8 +79,43 @@ INSERT INTO `categorias` (`id`, `tipo`, `fecha_creacion`, `fecha_eliminacion`) V
 CREATE TABLE `envios` (
   `id` int(11) NOT NULL,
   `id_pedido` int(11) NOT NULL,
-  `direccion` varchar(50) NOT NULL
+  `id_estado` int(11) NOT NULL DEFAULT 5,
+  `id_provincia` int(11) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `ciudad` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `envios`
+--
+
+INSERT INTO `envios` (`id`, `id_pedido`, `id_estado`, `id_provincia`, `direccion`, `ciudad`) VALUES
+(11, 42, 5, 1, 'Av. Garay 2235 4° F', 'CABA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estados`
+--
+
+CREATE TABLE `estados` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(50) NOT NULL,
+  `para` enum('pedido','envio','todos') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `estados`
+--
+
+INSERT INTO `estados` (`id`, `descripcion`, `para`) VALUES
+(1, 'Nuevo', 'pedido'),
+(2, 'En proceso', 'pedido'),
+(3, 'Listo', 'pedido'),
+(4, 'Cancelado', 'pedido'),
+(5, 'Pendiente', 'envio'),
+(6, 'En transito', 'envio'),
+(7, 'Entregado', 'todos');
 
 -- --------------------------------------------------------
 
@@ -61,26 +125,45 @@ CREATE TABLE `envios` (
 
 CREATE TABLE `pedidos` (
   `id` int(11) NOT NULL,
+  `id_estado` int(11) NOT NULL,
   `dni` int(11) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
+  `nombre` varchar(35) NOT NULL,
+  `apellido` varchar(35) NOT NULL,
   `telefono` int(11) NOT NULL,
-  `tipo` varchar(15) NOT NULL,
-  `estado` varchar(15) NOT NULL,
   `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
   `fecha_entrega` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id`, `id_estado`, `dni`, `nombre`, `apellido`, `telefono`, `fecha_creacion`, `fecha_entrega`) VALUES
+(42, 2, 47030345, 'Massimox', 'Pozzutox', 1140580368, '2023-11-27 04:54:28', NULL),
+(43, 1, 11222222, 'qwedrx', 'tgqwr', 1123232323, '2023-11-27 04:56:11', NULL);
+
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pedidos_productos`
+-- Estructura de tabla para la tabla `pedido_producto`
 --
 
-CREATE TABLE `pedidos_productos` (
+CREATE TABLE `pedido_producto` (
   `id_pedido` int(11) NOT NULL,
-  `id_producto` int(11) NOT NULL
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pedido_producto`
+--
+
+INSERT INTO `pedido_producto` (`id_pedido`, `id_producto`, `cantidad`) VALUES
+(42, 37, 1000),
+(42, 39, 70),
+(43, 36, 35),
+(43, 37, 1),
+(43, 39, 23);
 
 -- --------------------------------------------------------
 
@@ -90,21 +173,44 @@ CREATE TABLE `pedidos_productos` (
 
 CREATE TABLE `productos` (
   `id` int(11) NOT NULL,
-  `id_categoria` int(11) DEFAULT NULL,
   `nombre` varchar(50) NOT NULL,
   `stock` smallint(6) DEFAULT NULL,
   `precio` float NOT NULL,
   `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
-  `fecha_eliminacion` datetime DEFAULT NULL,
-  `rutimapro` varchar(20) NOT NULL
+  `fecha_eliminacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `id_categoria`, `nombre`, `stock`, `precio`, `fecha_creacion`, `fecha_eliminacion`, `rutimapro`) VALUES
-(1, 4, 'Prueba', 15, 5000, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '20231108001858.jpg');
+INSERT INTO `productos` (`id`, `nombre`, `stock`, `precio`, `fecha_creacion`, `fecha_eliminacion`) VALUES
+(36, 'Prueba0', 245, 8000, '2023-11-25 06:02:29', NULL),
+(37, 'Mesa 2m x 70cm x 60cm', 1385, 245412, '2023-11-25 06:13:28', NULL),
+(38, 'WEWER', 245, 6754, '2023-11-25 06:13:46', '2023-11-25 06:33:04'),
+(39, 'Tabla 90x90', 1441, 15000, '2023-11-25 06:21:19', NULL),
+(40, 'ProductoAhoraConCate', 30, 1550, '2023-11-25 06:32:11', NULL),
+(41, 'aaa', 124, 6536, '2023-11-25 06:35:20', NULL),
+(42, 'PruebaJEJOx', 190, 2000, '2023-11-26 16:24:29', '2023-11-26 16:26:52');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincias`
+--
+
+CREATE TABLE `provincias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `provincias`
+--
+
+INSERT INTO `provincias` (`id`, `nombre`) VALUES
+(1, 'Ciudad Autónoma de Buenos Aires'),
+(2, 'Buenos Aires');
 
 -- --------------------------------------------------------
 
@@ -118,16 +224,16 @@ CREATE TABLE `usuarios` (
   `apellido` varchar(30) DEFAULT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(40) NOT NULL,
-  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
-  `fecha_eliminacion` datetime DEFAULT NULL
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `username`, `password`, `fecha_creacion`, `fecha_eliminacion`) VALUES
-(1, NULL, NULL, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', '2023-11-09 01:51:51', NULL);
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `username`, `password`, `fecha_creacion`) VALUES
+(6, 'Massimo', 'Pozzuto', 'MassiPozzuto', '8cb2237d0679ca88db6464eac60da96345513964', '2023-11-27 09:02:52'),
+(7, '', NULL, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', '2023-11-27 09:09:33');
 
 --
 -- Índices para tablas volcadas
@@ -140,31 +246,53 @@ ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `categoria_producto`
+--
+ALTER TABLE `categoria_producto`
+  ADD PRIMARY KEY (`id_categoria`,`id_producto`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
 -- Indices de la tabla `envios`
 --
 ALTER TABLE `envios`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_envios__pedidos` (`id_pedido`);
+  ADD KEY `fk_envios__pedidos` (`id_pedido`),
+  ADD KEY `fk_envios_provincias` (`id_provincia`),
+  ADD KEY `fk_envios_estados` (`id_estado`);
+
+--
+-- Indices de la tabla `estados`
+--
+ALTER TABLE `estados`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pedidos_estados` (`id_estado`);
 
 --
--- Indices de la tabla `pedidos_productos`
+-- Indices de la tabla `pedido_producto`
 --
-ALTER TABLE `pedidos_productos`
-  ADD KEY `fk_pedidos_productos__pedidos` (`id_pedido`),
+ALTER TABLE `pedido_producto`
+  ADD PRIMARY KEY (`id_pedido`,`id_producto`),
   ADD KEY `fk_pedidos_productos__productos` (`id_producto`);
 
 --
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_productos__categorias` (`id_categoria`) USING BTREE;
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -180,54 +308,76 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `envios`
 --
 ALTER TABLE `envios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `estados`
+--
+ALTER TABLE `estados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT de la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `categoria_producto`
+--
+ALTER TABLE `categoria_producto`
+  ADD CONSTRAINT `categoria_producto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`),
+  ADD CONSTRAINT `categoria_producto_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`),
+  ADD CONSTRAINT `fk_id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
+
+--
 -- Filtros para la tabla `envios`
 --
 ALTER TABLE `envios`
-  ADD CONSTRAINT `fk_envios__pedidos` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`);
+  ADD CONSTRAINT `fk_envios__pedidos` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`),
+  ADD CONSTRAINT `fk_envios_estados` FOREIGN KEY (`id_estado`) REFERENCES `estados` (`id`),
+  ADD CONSTRAINT `fk_envios_provincias` FOREIGN KEY (`id_provincia`) REFERENCES `provincias` (`id`);
 
 --
--- Filtros para la tabla `pedidos_productos`
+-- Filtros para la tabla `pedidos`
 --
-ALTER TABLE `pedidos_productos`
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `fk_pedidos_estados` FOREIGN KEY (`id_estado`) REFERENCES `estados` (`id`);
+
+--
+-- Filtros para la tabla `pedido_producto`
+--
+ALTER TABLE `pedido_producto`
   ADD CONSTRAINT `fk_pedidos_productos__pedidos` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`),
   ADD CONSTRAINT `fk_pedidos_productos__productos` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`);
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `fk_productos_categorias` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
